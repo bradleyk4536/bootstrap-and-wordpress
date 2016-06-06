79d997c550ffaee41a739df172d357fcb26a8ac2
Kenneth Bradley <k_bnet570@verizon.net>
2016-06-06 06:04:35 -0400

    Finished featurette section of index.php

--- index.php
+++ index.php
@@ -187,7 +187,7 @@
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2">
 				<h2>Watch the Course Introduction</h2>
-
+				<iframe width="100%" height="415" src="//www.youtube.com/embed/69nEzHLy_tM" frameborder="0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
