bf858511366ec29098b180d5b114dfdaf6a64818
Kenneth Bradley <k_bnet570@verizon.net>
2016-06-06 08:36:07 -0400

    Added signup section

--- index.php
+++ index.php
@@ -303,7 +303,16 @@
 	</div>
 </section>
 <!--SIGNUP SECTION-->
-<section></section>
+<section id="signup" data-type="background" data-speed="4">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Are you ready to take you coding skills to the <strong>next level</strong>?</h2>
+				<p><a href="#" class="btn btn-lg btn-block btn-success">Sign me up</a></p>
+			</div>
+		</div>
+	</div>
+</section>
 <!--FOOTER SECTION-->
 <footer></footer>
 <!--MODAL SECTION-->
