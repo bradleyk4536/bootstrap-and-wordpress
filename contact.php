bd535d36f50111febdf1207cb9b2f4c025ec6774
Kenneth Bradley <k_bnet570@verizon.net>
2016-06-07 10:11:32 -0400

    Added contact page and styling

---
+++ contact.php
@@ -0,0 +1,55 @@
+<?php include ("includes/header.php"); ?>
+	<div class="full-width contact">
+<!--	FEATURE IMAGE-->
+		<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+		<h1 class="page-title">Contact</h1>
+		</section>
+<!--		CONTACT FORM-->
+		<div class="container">
+			<div class="row" id="primary">
+				<div id="content" class="col-sm-12">
+					<section class="main-content">
+						<p class="lead">
+							Have any questions. Feel free to get in touch.
+						</p>
+						<form action="" role="form" class="clearfix">
+							<div class="row">
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="contact-name" class="sr-only">Name</label>
+										<input type="text" class="form-control input-lg" id="contact-name" placeholder="Your name">
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="contact-email" class="sr-only">Email</label>
+										<input type="email" class="form-control input-lg" id="contact-email" placeholder="Your email">
+									</div>
+								</div>
+								<div class="col-sm-12">
+									<div class="form-group">
+										<label for="contact-words" class="sr-only">Name</label>
+										<textarea name="" id="" cols="30" rows="3" class="form-control input-lg" id="contact-words" placeholder="Your message"></textarea>
+									</div>
+								</div>
+							</div>
+							<input type="submit" class="btn btn-info btn-lg pull-right" value="Get in touch &raquo;">
+						</form>
+					</section>
+				</div>
+			</div>
+		</div>
+	<!--SIGNUP SECTION-->
+		<section id="signup" data-type="background" data-speed="4">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<h2>Are you ready to take you coding skills to the <strong>next level</strong>?</h2>
+						<p><a href="#" class="btn btn-lg btn-block btn-success">Sign me up</a></p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+<!--FOOTER SECTION-->
+<?php include ("includes/footer.php"); ?>
